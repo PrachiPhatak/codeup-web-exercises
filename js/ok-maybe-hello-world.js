af94d94c630bb803eb05278e37fe8a97bdec79ce
Prachi Phatak <prachiphatak@Prachis-MacBook-Pro.local>
2021-04-15 10:16:18 -0500

    Added operator assignment

---
+++ js/ok-maybe-hello-world.js
@@ -0,0 +1,16 @@
+"use strict";
+// MINI EXERCISE
+// TODO: I demand a haiku! Using alert, write a haiku!
+//     -> The entire poem consists of just three lines, with 17 syllables in total.
+//     -> The first line is 5 syllables.
+//     -> The second line is 7 syllables.
+//     -> haha The third line is 5 syllables.
+alert("An old silent pond...\n" +
+    "A frog jumps into the pond,\n" +
+    "splash! Silence again.");
+
+var movie =  prompt("What is your favorite color?");
+
+alert(movie);
+
+
---
+++ types-operator.js
@@ -0,0 +1,9 @@
+"use strict";
+// Mini Exercise - Numbers and Math
+// TODO: console.log() five equations using each arithmetic operator (+,-,*,/,%) which EACH will equal 4
+
+console.log("Addition +:"+(2 +2));
+console.log("Subtraction -:"+(6-2));
+console.log("Multiplication +:"+(2 *2));
+console.log("Division /:"+(8/2));
+console.log("Modulus %:"+(14 % 10));
