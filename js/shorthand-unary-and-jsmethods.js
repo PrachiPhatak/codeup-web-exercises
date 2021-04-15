924ca36e2b514a2ff5605b4eddf4366b473f3458
PrachiPhatak <phatak.prachi@gmail.com>
2021-04-15 16:28:34 -0500

    Adds shorthand unary operator exercises during class.

---
+++ js/shorthand-unary-and-jsmethods.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//Answer will be 10 for every shorthand unary
+var result = 15;
+console.log(result += -5);
+console.log("The value of variable 'result' is "+ result);
+console.log(result -= 2);
+console.log("The value of variable 'result' is "+ result);
+console.log(result *= 5);
+console.log("The value of variable 'result' is "+ result);
+console.log(result /= 4);
+console.log("The value of variable 'result' is "+ result);
+console.log(result %= 11);
+console.log("The final value of variable 'result' is "+ result);
