867ce3c140061c48640303ffd00d49d8221a105d
Prachi Phatak <prachi@github.com>
2021-04-15 11:50:54 -0500

    Moved javascipt files to js folder. Added logical example.

---
+++ js/logical-operator.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var hasFur = true;
+var hasWhiskers = true;
+var nameIsCoda = true;
+
+if(hasFur && hasWhiskers && nameIsCoda){
+    console.log("It's my dog");
+}
--- js/types-operator.js
+++ js/types-operator.js
@@ -7,3 +7,11 @@ console.log("Subtraction -:"+(6-2));
 console.log("Multiplication +:"+(2 *2));
 console.log("Division /:"+(8/2));
 console.log("Modulus %:"+(14 % 10));
+
+
+console.log("Type of 155 is : "+ typeof(155));
+console.log("Type of -345 is : "+ typeof(-345));
+console.log("Type of 0 is : "+ typeof(0));
+console.log("Type of 1.85 is : "+ typeof(1.85));
+
+console.log("Type of Prachi155 is : "+ typeof("Prachi155"));
