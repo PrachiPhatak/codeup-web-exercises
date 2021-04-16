6baab8cab3b913ccc09ecec4f50d0bee42de5ee7
PrachiPhatak <phatak.prachi@gmail.com>
2021-04-16 15:22:07 -0500

    Adds function exercise.

--- js/Intro_final_3c.js
+++ js/Intro_final_3c.js
@@ -1,2 +1,67 @@
-"use_strict";
+    "use_strict";
 
+    var class1;
+    class1 = {
+        id: 1,
+        name: "Computer Science",
+        capacity: 4,
+        enrolled_students: [],
+        start_time: new Date().setTime(10),
+        end_time: new Date().setTime(12),
+        isEnrolled: function (student) {
+            for (var i = 0; i < this.enrolled_students.length; i++) {
+                var tempStudent = this.enrolled_students[i];
+                console.log("here: "+tempStudent.id)
+                // if(this.enrolled_students[i].id === student.id){
+                //     console.log(student.id)
+                //     return true;
+                // }else {
+                //     console.log("In else")
+                //     return false;
+                // }
+            }
+            return false;
+        },
+        enroll: function (student) {
+            if (this.enrolled_students.length >= this.capacity) {
+                console.log(this.name + " Class is full.")
+                console.log("Apologies, "+ student.name + " will not be enrolled.");
+                return false;
+            } else {
+                if (this.isEnrolled(student)) {
+                    console.log(student.name +" is already enrolled in "+ this.name);
+                    return false;
+                } else {
+                    this.enrolled_students.push(student);
+                    console.log(student.name + " is enrolled in " + this.name);
+                    return true;
+                }
+            }
+        },
+
+        info: function () {
+            console.log("***********   "+ this.name + "    ************");
+            console.log("Capacity: " + this.capacity);
+            console.log("Time: " + this.start_time + " - " + this.end_time);
+            console.log("Student enrolled: " + this.enrolled_students.length)
+            this.enrolled_students.forEach(student1 => console.log("--"+student1.name))
+            console.log("****************************************")
+        }
+    };
+    var student1 = {
+        id: 1,
+        name: "Prachi"
+    };
+    var student2 = {
+        id: 2,
+        name: "xyz"
+    };
+    var student3 = {
+        id: 3,
+        name: "abc"
+    };
+    class1.enroll(student1)
+    class1.enroll(student2)
+    class1.enroll(student2)
+    class1.enroll(student3)
+    class1.info();
---
+++ js/miniExercise1.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function product(num1, num2){
+    return num1 * num2;
+}
+function dividend (num1, num2){
+    return num1 / num2;
+}
+
+console.log(product(32,4));
+console.log(dividend(30,4));
