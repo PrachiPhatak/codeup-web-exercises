0ad391016001a7238281d07f80a8eb38c3777257
PrachiPhatak <phatak.prachi@gmail.com>
2021-04-26 16:57:26 -0500

    Adds mini exercise

--- js/mini-array2.js
+++ js/mini-array2.js
@@ -6,10 +6,40 @@
 //      -> add the remainingDay of the week to the old array
 //  -> When the loop completes, console.log() the completed day of the week array
 
-let days = ["Monday", "Tuesday", "Wednesday"];
+let weekDays = ["Monday", "Tuesday", "Wednesday"];
 let remainingDays = ["Thursday", "Friday", "Saturday", "Sunday"];
 
-remainingDays.forEach(remainingDay => {
-    days.push(remainingDay);
+remainingDays.forEach( day => {
+    weekDays.push(day);
 })
-console.log(days);
+console.log(weekDays);
+
+// TODO - MINI EXERCISE
+//  -> Let's reverse engineer our days of the week array
+//  -> Make sure to console.log in order to see your code's behavior!
+//  -> Write a function which takes in your complete days of the week array
+//      -> It will eventually return your reverse-engineered array
+//  -> Using a FOR LOOP (not .forEach())
+//      -> pop() each item off the days array
+//      -> use the returned value from pop() to unshift the element
+//      -> on to the new array
+//  -> HINT: You may find that the loop doesn't behave as expected
+//      -> console.log EVERY possible thing which could change as your loop runs
+//          -> consider how the methods you are using change state of the array
+
+let reversedWeekDays = [];
+weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday" , "Friday", "Saturday", "Sunday"]
+console.log(" 'weekDays' -- " + weekDays);
+console.log("reversedWeekDays' -- " + reversedWeekDays+ "\n");
+
+
+// for (let i = weekDays.length-1 ; i >= 0;  i--){
+//     reversedWeekDays.unshift(weekDays.pop());
+// }
+
+weekDays.forEach( ()=> {
+    reversedWeekDays.push(weekDays.splice(0))
+})
+
+console.log("Final 'weekDays' -- " + weekDays);
+console.log("Final 'reverseWeekDays' -- " + reversedWeekDays);
