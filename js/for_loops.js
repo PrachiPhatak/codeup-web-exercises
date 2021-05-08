d246e40ad9ed7eb0876b0bbd96b4cb5dc79ddec3
PrachiPhatak <phatak.prachi@gmail.com>
2021-05-08 13:14:04 -0500

    Changes file names

--- js/for_loops.js
+++ js/for_loops.js
@@ -98,38 +98,5 @@
     }
     //function call
     printNumberDecrementBy5();
-    console.log("//---------------------------------------------------------------------------------");
-    //TODO An ice cream seller can't go home until she sells all of her cones.
-    // First write enough code that generates a random number between 50 and 100
-    // representing the amount of cones to sell before you start your loop.
-    // Inside of the loop your code should generate another random number between 1 and 5,
-    // simulating the amount of cones being bought by her clients.
-    // Use a do-while loop to log to the console the amount of cones sold to each person.
-    // This is a way get the random numbers for this exercise.
 
-    function numberOfCones(){
-        return  Math.floor(Math.random() * 50) + 50;
-    }
-
-    function numberOfConesBought(){
-        return Math.floor(Math.random() * 5) + 1;
-    }
-
-    function sellCones(){
-        let totalCones = numberOfCones();
-        console.log(`I have ${totalCones} cones.`)
-        let perCustomer;
-        do{
-            perCustomer = numberOfConesBought();
-            if(perCustomer <= totalCones){
-                totalCones = totalCones - perCustomer;
-                console.log(`${perCustomer} cones sold. ${totalCones} left.`)
-            }else{
-                console.log(`Cannot sell you ${perCustomer}. I only have ${totalCones}`)
-            }
-        }while(totalCones > 0)
-        console.log('Yay! I sold all the cones!')
-    }
-    //Function call
-    sellCones();
 })()
