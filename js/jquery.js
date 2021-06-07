2e3182bd5dc7c789a0fd6a68580357d80005664e
PrachiPhatak <phatak.prachi@gmail.com>
2021-06-07 10:32:23 -0500

    mini exercise- added new code to change card header

--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,9 @@
 "use strict";
 $(document).keyup(function (event) {
     console.log(event.keyCode);
+    displayNoOfLives(event.keyCode);
 });
+function displayNoOfLives(keyStroke) {
+    $("#lives").text(keyStroke)
+    console.log()
+}
--- js/mini-jquery-casey.js
+++ js/mini-jquery-casey.js
@@ -1,32 +1,43 @@
 let jqElements = $('.jqClass');
 let backgroundBlue = 'lightblue';
 let backgroundReset = '';
-function addHoverEvents(elementsToBind, color1, color2){
-    let handlerBlueBckGnd = function(){
+
+function addHoverEvents(elementsToBind, color1, color2) {
+    let handlerBlueBckGnd = function () {
         elementsToBind.css('background-color', color1);
         $(this).css('background-color', color2);
     }
-    let handlerReset = function(){
+    let handlerReset = function () {
         elementsToBind.css('backgroundReset', color1);
         $(this).css('backgroundReset', color2);
     }
-    elementsToBind.hover(handlerBlueBckGnd,handlerReset);
+    elementsToBind.hover(handlerBlueBckGnd, handlerReset);
 }
 
-function addImageChangeEvents(){
-    let handlerBlueBckGnd = function(){
-        $(this).attr("src", "cover/jazz-music-rubber-duck.jpg");
+function addImageChangeEvents() {
+    let handlerDuck = function () {
+        $(this).find('.card-img-top').attr("src", "cover/jazz-music-rubber-duck.jpg");
     }
-    let handlerReset = function(){
-        $(this).attr("src", "cover/question.png");
+    let handlerReset = function () {
+        $(this).find('.card-img-top').attr("src", "cover/question.png");
     }
-    $('.col-md-3').find('.card-img-top').hover(handlerBlueBckGnd,handlerReset);
+    $('.col-md-3').hover(handlerDuck, handlerReset);
 }
 
+function changeCardText() {
+    let handlerDuck = function () {
+        $(this).find('.card-title').text("Here");
+    }
+    let handlerReset = function () {
+        $(this).find('.card-title').text("Card");
+    }
+    $('.col-md-3').hover(handlerDuck, handlerReset);
+}
 
 // make sure the DOM is loaded first
-$(document).ready(function() {
+$(document).ready(function () {
     addHoverEvents(jqElements, backgroundReset, backgroundBlue);
     addHoverEvents($(".card"), backgroundReset, backgroundBlue);
     addImageChangeEvents();
+    changeCardText();
 });
